e97de3605a9de505a6fed8f18892788ad24d1e7d
Abusufiyan Akkalkotkar <98733918+abusufiyanakkalkotkar@users.noreply.github.com>
2024-11-22 19:49:07 +0530

    Update Amazon.java

--- EXCELR_PROJECT-2/src/test/java/amazon_website/Amazon.java
+++ EXCELR_PROJECT-2/src/test/java/amazon_website/Amazon.java
@@ -15,78 +15,78 @@ public class Amazon {
 
 	@BeforeMethod
 	public void setUp() {
-	driver = new ChromeDriver();
-    driver.manage().window().maximize();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
     }
 	@Test
 	public void test1() throws InterruptedException {
 		driver.get("https://www.amazon.in/");
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//span[@class='nav-line-2 ']")).click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//input[@type='email']")).sendKeys("9021598876");
-	    Thread.sleep(2000);
-	    driver.findElement(By.id("continue")).click();
-	    Thread.sleep(3000);
-	    driver.findElement(By.id("continue")).click();
-	    Thread.sleep(2000);
-	    String str = JOptionPane.showInputDialog("Enter verification code");
-	    WebElement element = driver.findElement(By.xpath("//input[@type='text']"));
-	    element.sendKeys(str);
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//input[@aria-label='Verify OTP Button']")).click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//span[@class='hm-icon-label' and text()='All']")).click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//a[@class='hmenu-item' and text()='Best Sellers']")).click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("(//div[@class='p13n-sc-truncate-desktop-type2  p13n-sc-truncated'])[01]")).click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//button[@class='a-button-text' and text()=' Add to Cart ']")).click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//span[@class='hm-icon-label' and text()='All']")).click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//a[@class='hmenu-item' and text()='Sign Out']")).click();
-	    Thread.sleep(2000);
-	    driver.close();
+	    	Thread.sleep(2000);
+	    	driver.findElement(By.xpath("//input[@type='email']")).sendKeys("9021598876");
+		Thread.sleep(2000);    
+		driver.findElement(By.id("continue")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.id("continue")).click();
+		Thread.sleep(2000);
+	        String str = JOptionPane.showInputDialog("Enter verification code");
+		WebElement element = driver.findElement(By.xpath("//input[@type='text']"));
+		element.sendKeys(str);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@aria-label='Verify OTP Button']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[@class='hm-icon-label' and text()='All']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[@class='hmenu-item' and text()='Best Sellers']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//div[@class='p13n-sc-truncate-desktop-type2  p13n-sc-truncated'])[01]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[@class='a-button-text' and text()=' Add to Cart ']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[@class='hm-icon-label' and text()='All']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[@class='hmenu-item' and text()='Sign Out']")).click();
+		Thread.sleep(2000);
+		driver.close();
 	}
 	@Test
 	public void test2() throws InterruptedException {
 		driver.get("https://www.amazon.in/");
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//span[@class='nav-line-2 ']")).click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//input[@type='email']")).sendKeys("9021598876");
-	    Thread.sleep(2000);
-	    driver.findElement(By.id("continue")).click();
-	    Thread.sleep(3000);
-	    driver.findElement(By.id("continue")).click();
-	    Thread.sleep(2000);
-	    String str = JOptionPane.showInputDialog("Enter verification code");
-	    WebElement element = driver.findElement(By.xpath("//input[@type='text']"));
-	    element.sendKeys(str);
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//input[@aria-label='Verify OTP Button']")).click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//span[@class='hm-icon-label' and text()='All']")).click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//a[@class='hmenu-item' and text()='Best Sellers']")).click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("(//div[@class='p13n-sc-truncate-desktop-type2  p13n-sc-truncated'])[01]")).click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//a[@class='a-button-text a-text-center' and text()= ' Go to Cart ' ]")).click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("(//i[@class='a-icon a-icon-checkbox'])[2]")).click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("(//input[@value='Delete'])[1]")).click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//span[@class='hm-icon-label' and text()='All']")).click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//a[@class='hmenu-item' and text()='Sign Out']")).click();
-	    Thread.sleep(2000);
+	        Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("9021598876");
+		Thread.sleep(2000);
+		driver.findElement(By.id("continue")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.id("continue")).click();
+		Thread.sleep(2000);
+		String str = JOptionPane.showInputDialog("Enter verification code");
+		WebElement element = driver.findElement(By.xpath("//input[@type='text']"));
+		element.sendKeys(str);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@aria-label='Verify OTP Button']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[@class='hm-icon-label' and text()='All']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[@class='hmenu-item' and text()='Best Sellers']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//div[@class='p13n-sc-truncate-desktop-type2  p13n-sc-truncated'])[01]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[@class='a-button-text a-text-center' and text()= ' Go to Cart ' ]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//i[@class='a-icon a-icon-checkbox'])[2]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//input[@value='Delete'])[1]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[@class='hm-icon-label' and text()='All']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[@class='hmenu-item' and text()='Sign Out']")).click();
+		Thread.sleep(2000);
 	}
 	@AfterClass
-	public void after() {
+	public void after() {	
 		driver.quit();
 	}	
 }
